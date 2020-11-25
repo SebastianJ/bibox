d750d71cc774ffb85f6df53b9b0ed83032a003de
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-11-25 10:58:32 +0100

    Updates

--- lib/bibox/rest/private/contracts/orders.rb
+++ lib/bibox/rest/private/contracts/orders.rb
@@ -17,6 +17,27 @@ module Bibox
             response    =   parse(post("/cquery", data: payload, options: options))&.fetch("result", [])&.first&.fetch("result", [])
           end
           
+          def perform_contracts_order(pair:, options: {})
+            params      =   {
+              pair:           pair,
+              order_type:     Bibox::Models::Order::ORDER_TYPES[order_type],
+              order_side:     Bibox::Models::Order::ORDER_SIDES[order_side],
+              pay_bix:        pay_fees_with_bix.eql?(true) ? 1 : 0,
+              price:          price,
+              amount:         amount,
+              money:          (price * amount),
+            }
+          
+            payload     =   [
+              {
+                cmd:  "order/open",
+                body: params
+              }
+            ]
+          
+            response    =   parse(post("/ctrade", data: payload, options: options))&.fetch("result", [])&.first&.fetch("result", {})
+          end
+          
         end
       end
     end
